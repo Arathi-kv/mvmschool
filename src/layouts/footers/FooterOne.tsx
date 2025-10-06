6776fcc78964f694ce4450a2559edb61a5587692
Arathi-k-v <arathi@signroots.com>
2025-10-06 11:34:58 +0530

    updated

--- src/layouts/footers/FooterOne.tsx
+++ src/layouts/footers/FooterOne.tsx
@@ -1,103 +1,122 @@
+"use client";
 
-
-import Link from 'next/link';
-import React from 'react';
+import Link from "next/link";
+import Image from "next/image";
+import React from "react";
 
 const FooterOne = ({ style_2 }: any) => {
   return (
-    <>
-      <footer>
-        {/* <!-- footer-area-start --> */}
-        <div className={`it-footer__area it-footer__space ${style_2 ? "black-bg" : "theme-bg"} z-index`}>
-          <div className="container">
-            <div className="row">
-              <div className="col-xl-3 col-lg-4 col-md-6 mb-30 wow itfadeUp" data-wow-duration=".9s"
-                data-wow-delay=".3s">
-                <div className="it-footer__widget footer-col-1">
-                  <div className="it-footer__logo">
-                    <Link href="/">
-                      <img src="assets/img/logo/footer-logo.png" alt="rk-theme" />
-                    </Link>
-                  </div>
-                  <div className="it-footer__text pb-15">
-                    <p>M.V.M.R.H.S.S – Quality education for all, from K.G. to Higher Secondary </p>
-                  </div>
-                 
+    <footer>
+      {/* <!-- footer-area-start --> */}
+      <div className={`it-footer__area it-footer__space ${style_2 ? "black-bg" : "theme-bg"} z-index`}>
+        <div className="container">
+          <div className="row">
+            {/* Footer Logo & Text */}
+            <div className="col-xl-3 col-lg-4 col-md-6 mb-30 wow itfadeUp" data-wow-duration=".9s" data-wow-delay=".3s">
+              <div className="it-footer__widget footer-col-1">
+                <div className="it-footer__logo">
+                  <Link href="/">
+                    <Image
+                      src="/assets/img/logo/footer-logo.png"
+                      alt="rk-theme"
+                      width={150}
+                      height={50}
+                      priority
+                    />
+                  </Link>
+                </div>
+                <div className="it-footer__text pb-15">
+                  <p>M.V.M.R.H.S.S – Quality education for all, from K.G. to Higher Secondary</p>
                 </div>
               </div>
-              <div className="col-xl-2 col-lg-4 col-md-6 mb-30 wow itfadeUp" data-wow-duration=".9s" data-wow-delay=".5s">
-                <div className="it-footer__widget footer-col-2">
-                  <h4 className="it-footer__widget-title">Usefull Links</h4>
-                  <div className="it-footer__list">
-                    <ul>
-                      <li><i className="flaticon-next"></i><a href="#">Facilities</a></li>
-                      <li><i className="flaticon-next"></i><a href="#">Management</a></li>
-                      <li><i className="flaticon-next"></i><a href="#">Curriculum</a></li>
-                      <li><i className="flaticon-next"></i><a href="#">Admission</a></li>
-                      <li><i className="flaticon-next"></i><a href="#">Contact</a></li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-              <div className="col-xl-4 col-lg-4 col-md-6 mb-30 wow itfadeUp" data-wow-duration=".9s"
-                data-wow-delay=".7s">
-                <div className="it-footer__widget footer-col-3">
-                  <h4 className="it-footer__widget-title">Our Updates</h4>
-                  <div className="it-footer__gallery-box">
-                    <div className="it-footer__gallery d-flex align-items-center">
-                      <img src="assets/img/footer/footer-sm-1-1.jpg" alt="rk-theme" />
-                      <div className="it-footer__gallery-content">
-                        <span className={`${style_2 ? "color-red" : ""}`}><i className="far fa-clock"></i>Admission</span>
-                        <p>Admissions open for the academic year.</p>
-                      </div>
-                    </div>
-                    <div className="it-footer__gallery d-flex align-items-center">
-                      <img src="assets/img/footer/footer-sm-1-2.jpg" alt="rk-theme" />
-                      <div className="it-footer__gallery-content">
-                        <span className={`${style_2 ? "color-red" : ""}`}><i className="far fa-clock"></i>Facilities</span>
-                        <p>From classrooms to labs, our facilities are designed for excellence.</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-xl-3 col-lg-4 col-md-6 mb-30 wow itfadeUp" data-wow-duration=".9s"
-                data-wow-delay=".9s">
-                <div className="it-footer__widget footer-col-4">
-                  <h4 className="it-footer__widget-title">Reach Us</h4>
-                  <div className="it-footer__input-box">
-                    <input type="text" placeholder="info@mvmrhss.com" />
-                    <div className={`it-footer__button-box ${style_2 ? "color-red" : ""}`}>
-                      <button><i className="fas fa-arrow-right"></i></button>
-                    </div>
-                  </div>
-                   <div className="it-footer__list">
-                    <ul>
-                      <li><i className="flaticon-next"></i><a href="#">+919567871424 , +917593078368</a></li>
-                  <br />
-                    </ul>
-                  </div>
+            </div>
 
-                  <div className="it-footer__social">
-                    <a href="#"><i className="fa-brands fa-facebook-f"></i></a> {" "}
-                    <a href="#"><i className="fa-brands fa-instagram"></i></a> {" "}
-                    <a href="#"><i className="fa-brands fa-linkedin-in"></i></a> {" "}
-                    <a href="#"><i className="fa-brands fa-twitter"></i></a> {" "}
+            {/* Useful Links */}
+            <div className="col-xl-2 col-lg-4 col-md-6 mb-30 wow itfadeUp" data-wow-duration=".9s" data-wow-delay=".5s">
+              <div className="it-footer__widget footer-col-2">
+                <h4 className="it-footer__widget-title">Useful Links</h4>
+                <div className="it-footer__list">
+                  <ul>
+                    <li><i className="flaticon-next"></i><a href="#">Facilities</a></li>
+                    <li><i className="flaticon-next"></i><a href="#">Management</a></li>
+                    <li><i className="flaticon-next"></i><a href="#">Curriculum</a></li>
+                    <li><i className="flaticon-next"></i><a href="#">Admission</a></li>
+                    <li><i className="flaticon-next"></i><a href="#">Contact</a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+            {/* Updates / Gallery */}
+            <div className="col-xl-4 col-lg-4 col-md-6 mb-30 wow itfadeUp" data-wow-duration=".9s" data-wow-delay=".7s">
+              <div className="it-footer__widget footer-col-3">
+                <h4 className="it-footer__widget-title">Our Updates</h4>
+                <div className="it-footer__gallery-box">
+                  <div className="it-footer__gallery d-flex align-items-center">
+                    <Image
+                      src="/assets/img/footer/footer-sm-1-1.jpg"
+                      alt="rk-theme"
+                      width={80}
+                      height={80}
+                      objectFit="cover"
+                    />
+                    <div className="it-footer__gallery-content">
+                      <span className={`${style_2 ? "color-red" : ""}`}><i className="far fa-clock"></i>Admission</span>
+                      <p>Admissions open for the academic year.</p>
+                    </div>
                   </div>
+                  <div className="it-footer__gallery d-flex align-items-center">
+                    <Image
+                      src="/assets/img/footer/footer-sm-1-2.jpg"
+                      alt="rk-theme"
+                      width={80}
+                      height={80}
+                      objectFit="cover"
+                    />
+                    <div className="it-footer__gallery-content">
+                      <span className={`${style_2 ? "color-red" : ""}`}><i className="far fa-clock"></i>Facilities</span>
+                      <p>From classrooms to labs, our facilities are designed for excellence.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {/* Contact / Social */}
+            <div className="col-xl-3 col-lg-4 col-md-6 mb-30 wow itfadeUp" data-wow-duration=".9s" data-wow-delay=".9s">
+              <div className="it-footer__widget footer-col-4">
+                <h4 className="it-footer__widget-title">Reach Us</h4>
+                <div className="it-footer__input-box">
+                  <input type="text" placeholder="info@mvmrhss.com" />
+                  <div className={`it-footer__button-box ${style_2 ? "color-red" : ""}`}>
+                    <button><i className="fas fa-arrow-right"></i></button>
+                  </div>
+                </div>
+                <div className="it-footer__list">
+                  <ul>
+                    <li><i className="flaticon-next"></i><a href="#">+919567871424 , +917593078368</a></li>
+                  </ul>
+                </div>
+                <div className="it-footer__social">
+                  <a href="#"><i className="fa-brands fa-facebook-f"></i></a>{" "}
+                  <a href="#"><i className="fa-brands fa-instagram"></i></a>{" "}
+                  <a href="#"><i className="fa-brands fa-linkedin-in"></i></a>{" "}
+                  <a href="#"><i className="fa-brands fa-twitter"></i></a>{" "}
                 </div>
               </div>
             </div>
           </div>
         </div>
-        {/* <!-- footer-area-end --> */}
+      </div>
+      {/* <!-- footer-area-end --> */}
 
-        {/* <!-- copyright-area-start --> */}
-        <div className={`it-copyright__area ${style_2 ? "black-bg" : "it-copyright__bg"}`}>
-          <div className="container">
-            <div className="row">
-              <div className="col-12 wow itfadeUp" data-wow-duration=".9s" data-wow-delay=".3s">
-                <div className="it-copyright__text text-center">
-                  <span>
+      {/* <!-- copyright-area-start --> */}
+      <div className={`it-copyright__area ${style_2 ? "black-bg" : "it-copyright__bg"}`}>
+        <div className="container">
+          <div className="row">
+            <div className="col-12 wow itfadeUp" data-wow-duration=".9s" data-wow-delay=".3s">
+              <div className="it-copyright__text text-center">
+                <span>
                   &copy; {new Date().getFullYear()}{" "}
                   <b className={`${style_2 ? "color-red" : ""}`}></b> All Rights Reserved. Powered by{" "}
                   <a
@@ -108,16 +127,13 @@ const FooterOne = ({ style_2 }: any) => {
                     signrOots
                   </a>
                 </span>
-                </div>
               </div>
             </div>
           </div>
         </div>
-        {/* <!-- copyright-area-end --> */}
-
-      </footer>
-
-    </>
+      </div>
+      {/* <!-- copyright-area-end --> */}
+    </footer>
   );
 };
 
--- src/layouts/headers/HeaderFour.tsx
+++ src/layouts/headers/HeaderFour.tsx
@@ -1,28 +1,36 @@
+"use client";
 
-"use client"
-
-import React, { useState } from 'react';
-import NavMenu from './Menu/nav-menu';
-import Link from 'next/link';
-import useSticky from '@/hooks/use-sticky';
-import Offcanvus from '@/components/common/Offcanvus';
+import React, { useState } from "react";
+import NavMenu from "./Menu/nav-menu";
+import Link from "next/link";
+import Image from "next/image";
+import useSticky from "@/hooks/use-sticky";
+import Offcanvus from "@/components/common/Offcanvus";
 
 const HeaderFour = () => {
-  const { sticky } = useSticky()
-  const [openSidebar, setOpenSidebar] = useState(false)
-
+  const { sticky } = useSticky();
+  const [openSidebar, setOpenSidebar] = useState(false);
 
   return (
     <>
       <header className="it-header-height">
         {/* <!-- header-area-start --> */}
-
-        <div id="header-sticky" className={`it-header__area ${sticky ? 'header-sticky' : ''}`}>
+        <div id="header-sticky" className={`it-header__area ${sticky ? "header-sticky" : ""}`}>
           <div className="container container-large">
             <div className="it-header__wrap p-relative">
+              {/* Desktop Logo */}
               <div className="it-header__logo d-none d-xl-block">
-                <Link href="/"><img src="assets/img/logo/school-logo.png" alt="rk-theme" /></Link>
+                <Link href="/">
+                  <Image
+                    src="/assets/img/logo/school-logo.png"
+                    alt="rk-theme"
+                    width={180}
+                    height={60}
+                    priority
+                  />
+                </Link>
               </div>
+
               <div className="row align-items-center">
                 <div className="col-xl-10 col-lg-6 col-md-6 col-6">
                   <div className="it-header__menu d-none d-xl-block text-center">
@@ -30,16 +38,26 @@ const HeaderFour = () => {
                       <NavMenu />
                     </nav>
                   </div>
+
+                  {/* Mobile Logo */}
                   <div className="it-header__main-logo d-xl-none">
-                    <Link href="/"><img src="assets/img/logo/school-logo.png" alt="rk-theme" /></Link>
+                    <Link href="/">
+                      <Image
+                        src="/assets/img/logo/school-logo.png"
+                        alt="rk-theme"
+                        width={140}
+                        height={50}
+                        priority
+                      />
+                    </Link>
                   </div>
                 </div>
+
                 <div className="col-xl-2 col-lg-6 col-md-6 col-6">
                   <div className="it-header__right-box d-flex align-items-center justify-content-end">
-                    
                     <div className="it-header__bar d-xl-none">
-                      <button className="it-menu-bar">
-                        <span onClick={() => setOpenSidebar(!openSidebar)}><i className="fa-regular fa-bars"></i></span>
+                      <button className="it-menu-bar" onClick={() => setOpenSidebar(!openSidebar)}>
+                        <i className="fa-regular fa-bars"></i>
                       </button>
                     </div>
                   </div>
@@ -49,12 +67,9 @@ const HeaderFour = () => {
           </div>
         </div>
         {/* <!-- header-area-end --> */}
-
       </header>
 
       <Offcanvus openSidebar={openSidebar} setOpenSidebar={setOpenSidebar} />
-
-
     </>
   );
 };
