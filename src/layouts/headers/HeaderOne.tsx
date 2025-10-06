16c291354182080c65eaccf836477c8c4df6b077
Arathi-k-v <arathi@signroots.com>
2025-10-06 13:53:21 +0530

    update

--- src/layouts/headers/HeaderOne.tsx
+++ src/layouts/headers/HeaderOne.tsx
@@ -50,33 +50,34 @@ const HeaderOne = () => {
         <div id="header-sticky" className={`it-header__area ${sticky ? 'header-sticky' : ''}`}>
           <div className="container container-large">
             <div className="it-header__wrap p-relative">
-              
-              <div className="it-header__logo d-none d-xl-block">
-                <Link href="/"><img src="assets/img/logo/school-logo.png" alt="logo" /></Link>
-              </div>
-              <div className="row align-items-center">
-                <div className="col-xl-10 col-lg-6 col-md-6 col-6">
-                  <div className="it-header__menu d-none d-xl-block text-center">
-                    <nav className="it-menu-content">
-                      <NavMenu />
-                    </nav>
-                  </div>
-                  <div className="it-header__main-logo d-xl-none">
-                    <Link href="/"><img src="assets/img/logo/school-logo.png" alt="logo" /></Link>
-                  </div>
+            <div className="it-header__logo d-none d-xl-block">
+              <Link href="/"><img src="assets/img/logo/school-logo.png" alt="logo" /></Link>
+            </div>
+            <div className="row align-items-center">
+              <div className="col-xl-10 col-lg-6 col-md-6 col-6">
+                <div className="it-header__menu d-none d-xl-block text-center">
+                  <nav className="it-menu-content">
+                    <NavMenu />
+                  </nav>
                 </div>
-                <div className="col-xl-2 col-lg-6 col-md-6 col-6">
-                  <div className="it-header__right-box d-flex align-items-center justify-content-end">
-                    
-                    <div className="it-header__bar d-xl-none">
-                      <button className="it-menu-bar">
-                        <span onClick={() => setOpenSidebar(!openSidebar)}><i className="fa-regular fa-bars"></i></span>
-                      </button>
-                    </div>
+                <div className="it-header__main-logo d-xl-none">
+                  <Link href="/"><img src="assets/img/logo/school-logo.png" alt="logo" /></Link>
+                </div>
+              </div>
+              <div className="col-xl-2 col-lg-6 col-md-6 col-6">
+                <div className="it-header__right-box d-flex align-items-center justify-content-end">
+                  <div className="it-header__bar d-xl-none">
+                    <button className="it-menu-bar">
+                      <span onClick={() => setOpenSidebar(!openSidebar)}>
+                        <i className="fa-regular fa-bars"></i>
+                      </span>
+                    </button>
                   </div>
                 </div>
               </div>
             </div>
+          </div>
+
           </div>
         </div>
         {/* <!-- header-area-end --> */}
