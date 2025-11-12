9f897c3af2585e9bc8b9cca6e2d30e6c31d4fa79
Arathi-k-v <arathi@signroots.com>
2025-11-12 11:50:22 +0530

    gallery update

--- src/components/about/AboutHomeTwo.tsx
+++ src/components/about/AboutHomeTwo.tsx
@@ -11,23 +11,24 @@ const AboutHomeTwo = () => {
           <div className="row align-items-xl-start align-items-center">
             <div className="col-xxl-5 col-xl-6 col-lg-6">
               <div className="it-about-4__thumb-box">
-                <img src="assets/img/about/about-4-1.png" alt="theme" />
+                <img src="assets/img/about/about1.jpg" alt="theme" />
               </div>
             </div>
             <div className="col-xxl-7 col-xl-6 col-lg-6 wow itfadeRight" data-wow-duration=".9s" data-wow-delay=".5s">
               <div className="it-about-3__right-wrap it-about-4__right-wrap">
                 <div className="it-about-3__section-box">
-                  <span className="it-section-subtitle-4">
+                  <span className="it-section-subtitle">
                     {/* <em>//</em>  */}
-                    KNOW ABOUT Tecch</span>
-                  <h4 className="it-section-title">Always Try to Deliver More
-                    Than You</h4>
-                  <h4>Focus on value education</h4>
-                  <h4>Separate Hostel for girls and boys</h4>
+                    KNOW ABOUT US</span>
+                  <h4 className="it-section-title"> M.V.M Higher Secondary School</h4>
                 </div>
-               
+                <p>
+                  M.V.M Higher Secondary School established in 1986, under Assabah Association Trust. a full-fledged Higher Secondary School starting from K.G. to Higher Secondary with reorganization from government of Kerala, 
+                  MVMRHSS believe that education is not merely the pursuit of academic excellence but the holistic development of every child. Established with a vision to nurture intellectually vibrant,
+                  morally upright,and socially responsible individuals, our school provides a dynamic learning environment that blends tradition with modern educational practices.
+                 </p>
                 
-                <Link className="it-red-btn hover-2" href="/about-us"><span>discover more</span></Link>
+                <Link className="it-btn hover-2" href="/about-us"><span>discover more</span></Link>
               </div>
             </div>
           </div>
